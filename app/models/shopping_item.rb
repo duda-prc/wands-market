043c7c81397dc6b12eef8de8ce3ab8d0ce1099bf
Eduarda Campos <eduarda.prc@gmail.com>
2022-07-28 19:26:35 -0300

    Changed validates shopping item quantity

--- app/models/shopping_item.rb
+++ app/models/shopping_item.rb
@@ -2,7 +2,7 @@ class ShoppingItem < ApplicationRecord
   belongs_to :shopping_cart
   belongs_to :wand
 
-  validates :quantity, numericality: { greater_than_or_equal_to: 0 }
+  validates :quantity, numericality: { greater_than_or_equal_to: 1 }
 
   def quantity_greater_than_stock
     errors.add(:quantity, 'is not available in stock')
