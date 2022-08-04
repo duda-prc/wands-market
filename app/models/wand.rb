66ca1ae7f4644ba216044f74344878162a8f464d
Eduarda Campos <eduarda.prc@gmail.com>
2022-08-04 17:15:43 -0300

    Search feature / faker seed improvement

--- app/models/wand.rb
+++ app/models/wand.rb
@@ -8,4 +8,14 @@ class Wand < ApplicationRecord
 
   validates :quantity, numericality: { greater_than_or_equal_to: 0 }
   validates :price, numericality: { greater_than_or_equal_to: 1 }
+
+  include PgSearch::Model
+  pg_search_scope :global_search,
+                  against: %i[name],
+                  associated_against: {
+                    user: %i[first_name last_name]
+                  },
+                  using: {
+                    tsearch: { prefix: true }
+                  }
 end
